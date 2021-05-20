a1f9f9b33e7546b900929e62bfa4d780f895f0c0
gonzaggb <gonzaggb@hotmail.com>
2021-05-19 21:26:57 -0300

    modificado app.js para correr en heroku

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express') // trae el modulo de express para poder montar el servidor
 const path = require('path') //requiere el modulo nativo path de node
 const app = express() 
+const port = process.env.PORT || 3000
 publicPath = path.join(__dirname,'public') // indica que la ruta contiene recursos estaticos para consumir de manera sencilla
 console.log(publicPath)
 
 app.use(express.static(publicPath));
 
-app.listen(3000,() => console.log('Servidor corriendo en el puerto 3000'))
+app.listen(port,() => console.log('Servidor corriendo en el puerto '+port))
 
 app.get('/',(req,res) => {
     
