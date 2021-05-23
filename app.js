f9e59867347c22c7f714caf1aa02eecbd73c7f4e
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-05-23 15:55:06 -0300

    cambio home a ejs y agregado de partials

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port))
 
 app.get('/', (req, res) => {
 
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
+    res.render('home')
 })
 
 app.get('/us', (req, res) => {
