428d69ac6b8462450ab3d92ba308fd4126c73129
gonzaggb <gonzaggb@hotmail.com>
2021-05-28 14:23:09 -0300

    _2_focal

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,32 +1,7 @@
-let productCheckout = [
-  {id: 1,
-  nombre: 'Lampara',
-  categoria: 'Plafon',
-  marca: 'Focal Glow',
-  potencia: '20W',
-  cct: '3000K',
-  precio: 1000 ,
-  dim: 'bluetooth',
-  cantidad: 2,
-  },
-  {id: 2,
-  nombre: 'Reflector',
-  categoria: 'Exterior',
-  potencia: '100W',
-  cct: '2000K',
-  precio: 1000 ,
-  dim: 'click',
-  cantidad: 1,
-  },
-  {id: 2,
-    nombre: 'Velador',
-    categoria: 'Interior',
-    potencia: '100W',
-    cct: '2000K',
-    precio: 1500 ,
-    dim: 'click',
-    cantidad: 5,
-    }
+let productCheckout = [  
+  {id: 1,  nombre: 'Lampara',  categoria: 'Plafon',  marca: 'Focal Glow',  potencia: '20W',  cct: '3000K',  precio: 1000 ,  dim: 'bluetooth',  cantidad: 2,  },
+  {id: 2,  nombre: 'Reflector',  categoria: 'Exterior', marca: 'Disney', potencia: '100W',  cct: '2000K',  precio: 1000 ,  dim: 'click',  cantidad: 1,  },
+  {id: 3,  nombre: 'Velador',    categoria: 'Interior', marca: 'Focal Glow',  potencia: '100W',  cct: '2000K',  precio: 1500 ,  dim: 'click',  cantidad: 5,  }
 ]
 
 
