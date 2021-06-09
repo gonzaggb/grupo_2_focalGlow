5c7093caaac628d7f81d2a794d48a0098b9d3a01
Mars <martinianol@outlook.com>
2021-06-09 17:24:15 -0300

    Home Category funcionando con info de base

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -3,14 +3,14 @@ const products = require('../models/product')
 
 const controller = {
   category: (req, res) => {
-    const id = req.params.id
-    let category = categories.findByPk(id)
-    let product = products.filterByCategory (category.name)
-    let dataCategory = {category , product}
-    
-    res.render('category.ejs', { dataCategory } )
-  },
+    const name = req.params.id
+    let category = categories.findByName(name)
+    let product = products.filterByCategory(category.name)
 
+    let dataCategory = { category, product }
+
+    res.render('category.ejs', { dataCategory })
+  },
 }
 
 module.exports = controller
