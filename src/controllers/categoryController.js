75a12ad422c88b71a66aef8b5bc051df8ab4aeda
gonzaggb <gonzaggb@hotmail.com>
2021-06-10 15:03:50 -0300

    modificada ruta de categoria para que muestre el nombre de la misma y el id, de este modo es lindo de cara al usuario y podemos trabajar con el id | actualizada la home

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -4,11 +4,9 @@ const products = require('../models/product')
 const controller = {
   category: (req, res) => {
     const name = req.params.id
-    let category = categories.findByName(name)
+    let category = categories.findByPk(name)
     let product = products.filterByCategory(category.name)
-
     let dataCategory = { category, product }
-
     res.render('category.ejs', { dataCategory })
   },
 }
