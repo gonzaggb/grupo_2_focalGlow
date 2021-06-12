8295b0ccf23277bf04716ef479308a6449ff70f3
Mars <martinianol@outlook.com>
2021-06-12 10:34:45 -0300

    Productos tienen los pdfs samples.

--- src/models/product.js
+++ src/models/product.js
@@ -56,16 +56,17 @@ module.exports = {
         case 'product_img':
           product.main_image = '/img/' + files[i].filename
           break
+        case 'image_slider':
+          product.image_slider.push('/img/' + files[i].filename)
+          break
+        case 'image_dimension':
+          product.image_dimension = '/img/' + files[i].filename
+          break
         case 'data_sheet':
           product.data_sheet = '/pdf/' + files[i].filename
           break
         case 'install_sheet':
           product.install_sheet = '/pdf/' + files[i].filename
-          break
-        case 'image_slider':
-          product.image_slider.push('/img/' + files[i].filename)
-        case 'image_dimension':
-          product.image_dimension = '/img/' + files[i].filename
           break
         default:
       }
