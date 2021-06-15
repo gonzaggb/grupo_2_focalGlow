4f256c1d188fa50628fca6804dc74f86a5eb9479
gonzaggb <gonzaggb@hotmail.com>
2021-06-15 20:12:20 -0300

    finalizada validación imagenes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -8,9 +8,20 @@ const { body } = require('express-validator')
 const validations = [
     body('name').notEmpty().withMessage('Debes poner tu nombre'),
     body('surname').notEmpty().withMessage('Debes poner tu apellido'),
-    body('email').notEmpty().withMessage('Debes poner tu email'),
+    body('email').notEmpty().withMessage('Debes poner tu email').bail().isEmail().withMessage("El email ingresado no es valido"),
     body('password').notEmpty().withMessage('Debes poner una contraseña'),
-    body('rePassword').notEmpty().withMessage('Debes confirmar la contraseña')
+    body('rePassword').notEmpty().withMessage('Debes confirmar la contraseña'),
+    body('profileImg').custom((value, {req}) => {
+        const file = req.file
+        const validExtension = ['.jpg', '.png', '.gif']
+        if(!file){
+            throw new Error ('Debes subir una imagen')
+        }
+        if(!validExtension.includes(path.extname(file.originalname))){
+            throw new Error (`Las extensions permitidas son ${validExtension.join(', ')}`)
+        }
+        return true
+    })
 
 ]
 
@@ -36,7 +47,7 @@ router.post('/login', userController.loginUser)
 //envia al usuario a la pagina de registro
 router.get('/registro', userController.newUser)
 //envia los datos de la pagina de registro al controlador
-router.post('/registro',upload.single('profile-img'), validations,  userController.create)
+router.post('/registro',upload.single('profileImg'), validations,  userController.create)
 
 //envia al usuario admin al listado de usuarios
 router.get('/',  userController.list) // la barra sola equivale a /users porque del app.js vengo con /users
