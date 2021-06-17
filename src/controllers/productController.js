5610bd53487b3364d8a7f2c2626da738fc275b04
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-06-17 00:30:42 -0300

    rancio

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,6 +2,8 @@
 const product = require('../models/product')
 // se requiere express-validator, pero la parte de validation result
 const {validationResult}= require('express-validator')
+const fs = require('fs')
+const path = require('path')
 
 const controller = {
   list: (req, res) => {
@@ -20,13 +22,11 @@ const controller = {
   },
   create: (req, res) => {
     let errors = validationResult(req)
-    if(errors.isEmpty()){
-      const productNew = req.body
-    const files = req.files
-
-    
-    product.create(productNew, files)
-    res.redirect('/product/list')
+      if(errors.isEmpty()){
+        const productNew = req.body
+        const files = req.files
+        product.create(productNew, files)
+        res.redirect('/product/list')
     } else {
       res.render('products/product-create.ejs',
        { errors: errors.array(),
@@ -37,25 +37,92 @@ const controller = {
   edit: (req, res) => {
     let id = req.params.id
     let productFound = product.findByPk(id)
+    
     res.render('products/product-edit.ejs', { productFound: productFound })
+    
   },
   update: (req, res) => {
     let data = req.body
     let id = req.params.id
     let productOriginal = product.findByPk(id)
     let {files} = req
+    
+    //
+    for (let i = 0; i < files.length; i++) {
+      switch (files[i].fieldname) {
+        case 'main_image':
+          if (undefined) {
+            data.main_image = productOriginal.main_image
+          } else {
+            data.main_image = ('/img/' + files[i].filename)
+          }
+          break
+        case 'image_slider':  
+        if (undefined) {
+            data.image_slider = productOriginal.image_slider
+          }else{
+              
+
+             files.forEach((e ,index) => {
+               console.log(productOriginal.image_slider[index])
+               e ? e = ('/img/' + e.filename) : productOriginal.image_slider[index] ;
+             }); 
+             
+        
+         /*  files[i][0] ? data.image_slider[0] = ('/img/' + files[i][0].filename) : productOriginal.imageSlider[0];
+          files[i][1] ?  data.image_slider[1] = ('/img/' + files[i][1].filename) : productOriginal.imageSlider[1];
+          files[i][2] ?  data.image_slider[2] = ('/img/' + files[i][2].filename) : productOriginal.imageSlider[2]; */
+          }  
+         
+
+        
+          
+          break
+        case 'image_dimension':
+          if (undefined) {
+            data.image_dimension = productOriginal.image_dimension
+          } else {
+            data.image_dimension = ('/img/' + files[i].filename)
+          }
+          break
+        case 'data_sheet':
+          if (undefined) {
+            data.image_dimension = productOriginal.image_dimension
+          } else {
+            data.image_dimension = ('/pdf/' + files[i].filename)
+          }
+          break
+        case 'install_sheet':
+          if (undefined) {
+            data.install_sheet = productOriginal.install_sheet
+          } else {
+            data.install_sheet = ('/pdf/' + files[i].filename)
+          }
+          break
+        default:
+      }
+
+    
    
     
-    console.log(data)
-    return
+    }
     product.update(data, id)
     res.redirect('/product/list')
   },
 
   delete: (req, res) => {
     let id = req.params.id
-    let productDelet = product.delete(id)
+    let productDelet = product.findByPk(id)
+    
+    fs.unlinkSync(path.join(__dirname, '../../public/', productDelet.main_image))
+    fs.unlinkSync(path.join(__dirname, '../../public/', productDelet.image_dimension))
+    fs.unlinkSync(path.join(__dirname, '../../public/', productDelet.data_sheet))
+    fs.unlinkSync(path.join(__dirname, '../../public/', productDelet.install_sheet))
+    
     res.redirect('/product/list')
+    product.delete(id)
+    
+    
   },
 }
 
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -7,11 +7,22 @@ const multer = require('multer')
 const path = require('path')
 //requerir express-validator
 const{body} = require('express-validator')
+//requerir fs
+const fs = require('fs')
+//requerir helper
+const { isFileImage } = require('../helpers/file')
 //agregar las validaciones
 const validateCreateForm = [
-  body('name').notEmpty().withMessage('* Falta agregar nombre de producto'),
-  body('qty').notEmpty().withMessage('* Falta agregar la cantidad de productos'),
-  body('price').notEmpty().withMessage('* Falta agregar el precio del producto'),
+  body('name')
+    .notEmpty()
+    .withMessage('* Falta agregar nombre de producto'),
+  body('qty')
+    .notEmpty()
+    .withMessage('* Falta agregar la cantidad de productos'),
+  body('price')
+    .notEmpty()
+    .withMessage('* Falta agregar el precio del producto'),
+  
 ]
 
 //aplicacion de multer
@@ -29,7 +40,33 @@ const storage = multer.diskStorage({
     cb(null, category + Date.now() + path.extname(file.originalname))
   },
 })
-const upload = multer({ storage })
+
+// para eliminar el archivo que sube multer
+const fileFilter = (req, file, cb) => {
+  const files = req.files
+  if (!files) {
+    cb(null, false)
+
+    // corta ejecución
+    return
+  }else (files)=>{
+     files.forEach(e => {
+       
+      fs.unlinkSync(e)
+    });
+
+    cb(null, false)
+  
+    // corta ejecución
+    return
+  }
+
+
+  // Si aceptamos el archivo
+  cb(null, true)
+
+}
+const upload = multer({ storage , fileFilter })
 
 //requiero el controlador de main
 const productController = require('../controllers/productController')
@@ -42,12 +79,12 @@ router.get('/detail/:id', productController.detail)
 
 //Create
 router.get('/add', productController.formNew) //formulario de creacion de producto
-router.post('/add', upload.any('product_img', 'data_sheet', 'install_sheet', 'image_slider', 'image_dimension'),validateCreateForm ,productController.create)
+router.post('/add', upload.any('main_image', 'data_sheet', 'install_sheet', 'image_slider', 'image_dimension'),validateCreateForm ,productController.create)
 // a donde va el producto creado
 
 //Update
 router.get('/:id/edit', productController.edit) //formulario de edicion de producto
-router.put('/:id/edit', upload.any('product_img', 'data_sheet', 'install_sheet', 'image_slider', 'image_dimension'), productController.update)
+router.put('/:id/edit', upload.any('main_image', 'data_sheet', 'install_sheet', 'image_slider', 'image_dimension'), productController.update)
 
 //Delete
 router.delete('/:id', productController.delete)
