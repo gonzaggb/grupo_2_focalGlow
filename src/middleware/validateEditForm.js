52dcd2f4744022fb90738a86577cf398f384094d
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-06-23 09:17:10 -0300

    agregado de validacion al edit form

--- src/middleware/validateEditForm.js
+++ src/middleware/validateEditForm.js
@@ -3,16 +3,42 @@ const { checkFieldImage, checkFieldPdf } = require('../helpers/checkFiles')
 
 //agregar las validaciones
 const validateEditForm = [
-    body('name').notEmpty().withMessage('Favor de indicar el nombre de producto'),
-    body('qty').notEmpty().withMessage('Favor de indicar la cantidad de productos'),
-    body('price').notEmpty().withMessage('Favor de indicar el precio del producto'),
-    body('source').notEmpty().withMessage('Favor de seleccionar una fuente'),
-    body('cct').notEmpty().withMessage('Favor de seleccionar el CCT'),
-    body('dim').notEmpty().withMessage('Favor de seleccionar el DIM'),
-    body('material').notEmpty().withMessage('Favor de seleccionar el material'),
-    body('optic').notEmpty().withMessage('Favor de seleccionar la óptica'),
-    body('category').notEmpty().withMessage('Favor de seleccionar una categoría'),
-    body('description').notEmpty().withMessage('Favor de incluir una descripción del producto'),
+    body('name')
+        .notEmpty()
+        .withMessage('Favor de indicar el nombre de producto')
+        .bail()
+        .isLength({ min: 5 })
+        .withMessage('Por favor un nombre más largo'),
+    body('qty')
+        .notEmpty()
+        .withMessage('Favor de indicar la cantidad de productos'),
+    body('price')
+        .notEmpty()
+        .withMessage('Favor de indicar el precio del producto'),
+    body('source')
+        .notEmpty()
+        .withMessage('Favor de seleccionar una fuente'),
+    body('cct')
+        .notEmpty()
+        .withMessage('Favor de seleccionar el CCT'),
+    body('dim')
+        .notEmpty()
+        .withMessage('Favor de seleccionar el DIM'),
+    body('material')
+        .notEmpty()
+        .withMessage('Favor de seleccionar el material'),
+    body('optic')
+        .notEmpty()
+        .withMessage('Favor de seleccionar la óptica'),
+    body('category')
+        .notEmpty()
+        .withMessage('Favor de seleccionar una categoría'),
+    body('description')
+        .notEmpty()
+        .withMessage('Favor de incluir una descripción del producto')
+        .bail()
+        .isLength({ min: 20 })
+        .withMessage('Por favor agregar una descripcion mas larga'),
     /* body('power').notEmpty().withMessage('Favor de incluir una potencia'), */
 ]
 
