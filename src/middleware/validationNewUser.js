7a91c72af51b6ed93a7f9464cda9c5648cac8a23
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-06-28 15:31:40 -0300

    Chequeado de usuario funcionando

--- src/middleware/validationNewUser.js
+++ src/middleware/validationNewUser.js
@@ -1,9 +1,18 @@
 const { body } = require('express-validator')
 const files = require('../helpers/files')
+const userModel = require ('../models/user')
 const validations = [
     body('first_name').notEmpty().withMessage('Debes poner tu nombre'),
     body('last_name').notEmpty().withMessage('Debes poner tu apellido'),
-    body('email').notEmpty().withMessage('Debes poner tu email').bail().isEmail().withMessage("El email ingresado no es valido"),
+    body('email').notEmpty().withMessage('Debes poner tu email').bail().isEmail().withMessage("El email ingresado no es valido").bail()
+    .custom((val)=>{
+        const userFound = userModel.findByField('email',val)
+        if(userFound){
+            return false
+        }
+        return true
+    }).withMessage('el usuario ya existe'),
+    
     body('password').notEmpty().withMessage('Debes poner una contraseña'),
     body('rePassword').notEmpty().withMessage('Debes confirmar la contraseña'),
     body('profileImg').custom((value, { req }) => {
