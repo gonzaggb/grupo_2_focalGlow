066e16aa974dc42713d33a4d3ed7b603ba88a174
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-07-01 09:52:15 -0300

    agregado de ejs del 404

--- src/app.js
+++ src/app.js
@@ -52,3 +52,8 @@ app.use('/', mainRoutes)
 app.use('/product', productRoutes)
 app.use('/categorias', categoryRoutes)
 app.use('/users', userRoutes)
+
+// Ruta 404
+app.use((req,res,next)=>{
+  res.status(404).render('404.ejs')
+})
