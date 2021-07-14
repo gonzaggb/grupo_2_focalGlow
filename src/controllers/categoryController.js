434c3e9ec00e4cc49ac194e1a5cf3a4708b2f0b5
Mars <martinianol@outlook.com>
2021-07-14 17:57:12 -0300

    Fixed bugs in categories

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -8,7 +8,6 @@ const controller = {
 
     const id = req.params.id
     let category = categories.findByPk(id)
-    console.log(category)
     let product = products.filterByCategory(category.name)
     let dataCategory = { category, product }
     res.render('category.ejs', { dataCategory })
