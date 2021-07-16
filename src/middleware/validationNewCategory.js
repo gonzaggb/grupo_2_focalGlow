a18ddb3b067024117273bf60c75a3ee938b470ef
Mars <martinianol@outlook.com>
2021-07-15 23:42:05 -0300

    ultimo commit del dia

--- src/middleware/validationNewCategory.js
+++ src/middleware/validationNewCategory.js
@@ -1,16 +1,33 @@
 const { body } = require('express-validator')
 const files = require('../helpers/files')
-const userModel = require('../models/user')
+const categoryModel = require('../models/category')
 const validations = [
-    body('name').notEmpty().withMessage('Debes ingresar el nombre de la categoría').bail(),
+	body('name')
+		.notEmpty().withMessage('Debes ingresar el nombre de la categoría').bail()
+		.isLength({ min: 5 }).withMessage('El nombre debe ser más largo')
 
-    body('coverImg').custom((value, { req }) => {
-        const file = req.file
-        if (file && !files.isFileImage(file.originalname)) {
-            throw new Error(`Ingrese un archivo que sea una imagen`)
-        }
-        return true
-    })
+		.custom((val) => {
+			const categoryFound = categoryModel.findByField('name', val)
+			if (categoryFound) {
+				return false
+			}
+			return true
+		}).withMessage('La categoría ya existe'),
+
+	body('image_cover').custom((value, { req }) => {
+		const file = req.files
+		if (file && !files.isFileImage(file.originalname)) {
+			throw new Error(`Ingrese un archivo que sea una imagen`)
+		}
+		return true
+	}),
+	body('iamger_home').custom((value, { req }) => {
+		const file = req.files
+		if (file && !files.isFileImage(file.originalname)) {
+			throw new Error(`Ingrese un archivo que sea una imagen`)
+		}
+		return true
+	})
 ]
 
 module.exports = validations
