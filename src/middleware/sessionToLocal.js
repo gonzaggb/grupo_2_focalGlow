ca05d73ad7e33f052e22b2be44b1f5a5383379b0
Mars <martinianol@outlook.com>
2021-07-17 01:54:51 -0300

    Bardo en sessionToLocal

--- src/middleware/sessionToLocal.js
+++ src/middleware/sessionToLocal.js
@@ -1,19 +1,34 @@
-//const user = require('../models/user')
 const { User } = require('../database/models')
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
+
 
 	if (req.session.logged) {
 		//FIXME
 		console.log(req.session.logged)
-		User.findByPk(req.session.logged)
-			.then(userFound => {
-				console.log('Soy la promesa ' + userFound)
+		const userFound = await User.findByPk(req.session.logged)
+			
 				//delete userFound.password
+				
 				res.locals.user = userFound
-			})
+			}
 
-
-	}
 	next()
+	}
+	
+
+
+/*
+const user = require('../models/user')
+module.exports = (req, res, next) => {
+
+    if (req.session.logged){
+        //FIXME
+        let userFound = user.findByPk(req.session.logged)
+        delete userFound.password
+        res.locals.user = userFound
+        
+    }
+    next()
 }
+*/
