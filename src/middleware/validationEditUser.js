e13122f45b62a41c6c0a2e07c2e707bd0ac18273
Mars <martinianol@outlook.com>
2021-07-18 23:21:58 -0300

    USERS CRUD DONE

--- src/middleware/validationEditUser.js
+++ src/middleware/validationEditUser.js
@@ -2,15 +2,20 @@ const { body } = require('express-validator')
 const files = require('../helpers/files')
 const { User } = require('../database/models')
 
+
 const validations = [
-	//FIXME
+
 	body('firstName').notEmpty().withMessage('El nombre no puede estar vacío'),
 	body('lastName').notEmpty().withMessage('El apellido no puede estar vacío'),
 	body('email').notEmpty().withMessage('Debes poner tu email').bail().isEmail().withMessage("El email ingresado no es valido").bail()
 		.custom(async (val, { req }) => {
 			const userFound = await User.findOne({ where: { email: val } })
-			const userAdmin = await User.findByPk(req.session.logged)
-			if (userFound && (userFound.id == req.session.logged || userAdmin.role == 'admin')) {
+			const userLogged = await User.findByPk(req.session.logged)
+
+
+			if (userFound && userFound.id == userLogged.id) {
+				return true
+			} else if (userLogged.role == 'admin' && userFound.id == req.body.id) {
 				return true
 			} else if (userFound) {
 				return Promise.reject('El usuario ya existe')
@@ -33,23 +38,14 @@ const validations = [
 		return true
 	}),
 
-	/*body('profileImg').custom((value, { req }) => {
+	body('profileImg').custom((value, { req }) => {
 		const file = req.file
-		if (!file) {
-			throw new Error('Debes subir una imagen')
-		} else if (!files.isFileImage(file.originalname)) {
+		if (file && !files.isFileImage(file.originalname)) {
 			throw new Error(`Ingrese un archivo que sea una imagen`)
 		}
 		return true
-	})*/
+	})
 
 ]
-
-
-
-
-
-
-
 
 module.exports = validations
