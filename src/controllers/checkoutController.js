4757a4bab507c3f99417350b4bf803ab3a77970e
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-08-07 17:35:57 -0300

    agregado de comentarios

--- src/controllers/checkoutController.js
+++ src/controllers/checkoutController.js
@@ -1,43 +1,43 @@
 const session = require('express-session')
-const {Product} = require('../database/models')
-const {Item}= require('../database/models')
-const {Feature}= require('../database/models')
-const Op= require('sequelize')
+const { Product } = require('../database/models')
+const { Item } = require('../database/models')
+const { Feature } = require('../database/models')
+const Op = require('sequelize')
 const { checkout } = require('./mainController')
 
 
 const controller = {
-    add: async function (req,res){
+    add: async function (req, res) {
         const product = await Product.findByPk(req.body.id)
-        const {cct , dim, optic, power}= req.body
+        const { cct, dim, optic, power } = req.body
         const productImage = await product.getImages()
         let mainImage = ''
         productImage.forEach(e => {
-            if(e.type == 'main'){
-                 mainImage = e.name
-               
+            if (e.type == 'main') {
+                mainImage = e.name
+
             }
         });
-        const productFeatureAux= await Feature.findAll({
-            attributes:['name'],
+        const productFeatureAux = await Feature.findAll({
+            attributes: ['name'],
             where: {
                 id: [cct, dim, optic, power]
             },
-            
-            
+
+
         })
         let productFeatures2 = []
-        productFeatureAux.forEach(e=>{
+        productFeatureAux.forEach(e => {
             productFeatures2.push(e.name)
         })
         let productFeatures = {
-            CCT :  'CCT: ' +productFeatures2[0],
-            DIM : 'DIM: '+ productFeatures2[1],
-            OPTIC : 'OPTIC: '+productFeatures2[2],
-            POWER : 'POWER: '+productFeatures2[3]
-        } 
-        
-        
+            CCT: 'CCT: ' + productFeatures2[0],
+            DIM: 'DIM: ' + productFeatures2[1],
+            OPTIC: 'OPTIC: ' + productFeatures2[2],
+            POWER: 'POWER: ' + productFeatures2[3]
+        }
+
+
         const productPrice = Number(product.price) //FIXME actualmente toma el precio del produco, tenemos que hacer que el precio se 
         //actualice en el front en base a las diferentes features y mandarlo por el body
         const quantity = Number(req.body.quantity)
@@ -47,37 +47,35 @@ const controller = {
             productName: product.name,
             productPrice,
             productDescription: product.description,
-            productFeatures: JSON.stringify(productFeatures),
+            productFeatures: JSON.stringify(productFeatures),//aca paso el objeto a string para que lo tome la DB
             productImage: mainImage,
             quantity,
             subTotal: quantity * productPrice,
             userId
 
         }
-        
-        
+
+
         await Item.create(item)
         res.redirect('/checkout')
     },
-    list: async (req,res)=>{
+    list: async (req, res) => {
         const productCheckout = await Item.findAll({
-            where:{
+            where: {
                 userId: res.locals.user.id
             }
 
-           
+
         })
-        console.log(productCheckout)
-        
-        let features = [] 
-        
-        const featuresaux =  productCheckout.forEach(e=>{
-           features.push(JSON.parse(e.productFeatures))
+
+        let features = []
+        const featuresaux = productCheckout.forEach(e => {
+            features.push(JSON.parse(e.productFeatures))//aca paso a objeto los strings de los features
         })
-         
-          
+
+
         //preguntar como hacer para que viaje con el nombre 
-        res.render('checkout.ejs', {productCheckout, features})
+        res.render('checkout.ejs', { productCheckout, features })
     }
     // validar que el usuario no pueda agregar dos productos iguales en items diferentes
     //tomar el precio de la db y no del front 
