d21002137f3cdb5856cea195137773603607beb3
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-08-11 00:42:01 -0300

    Api user id to test

--- src/controllers/API/usersApiController.js
+++ src/controllers/API/usersApiController.js
@@ -2,28 +2,42 @@ const {User} = require('../../database/models')
 
 const controller = {
     list: async(req,res)=> {
-        let users = await User.findAll()
+        let total = await User.findAll()
+        
+        total.forEach(element => {
+            user.push([id,name,email])
+        });
+        console.log(user)
+        
         let response = {
-            meta: {
-                status : 200,
-                total : users.length,
-                url: 'api/users'
-
+            count: {
+                total : total.length
             },
-            data:users
+            users: total
+            
+                
             
         }
         res.json(response)
     },
     detail : async(req,res)=>{
-        let {id} = req.params
-        let user = await User.findByPk(id)
+        let userToFind = req.params.id
+        let { id, firstName, lastName, email, profileImg } = await User.findByPk(userToFind)
+        let user = {
+            id,
+            firstName,
+            lastName,
+            email,
+            profileImg
+        }
+        
         let response = {
             meta: {
                 status: 200,
                 url: 'api/users/'+id
             },
             data: user
+            
         }
         res.json(response)
     }
