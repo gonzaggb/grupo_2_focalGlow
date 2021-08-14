2818795b457606d935a122c7e35caeed488e5690
Mars <martinianol@outlook.com>
2021-08-13 21:28:49 -0300

    Bug de User naming

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,7 +20,7 @@ const controller = {
     const oldValues = req.body
 
     if (!formValidation.isEmpty()) {
-      return res.render('users/user-login', { oldValues, errors: formValidation.mapped() })
+      return res.render('users/user-login.ejs', { oldValues, errors: formValidation.mapped() })
     }
     const { email, remember } = req.body
 
@@ -60,7 +60,7 @@ const controller = {
     if (!validationStatus.isEmpty()) {
 
       if (!req.file) { //valido que exista un archivo, en caso de no existir retorno los errores
-        return res.render('users/register.ejs', { errors: validationStatus.mapped(), oldData: req.body }) // se mapea para que devuelva como un objeto literal con sus respectivas propiedades
+        return res.render('users/user-register.ejs', { errors: validationStatus.mapped(), oldData: req.body }) // se mapea para que devuelva como un objeto literal con sus respectivas propiedades
       } else {
         if (isFileImage(req.file.originalname)) { // si existe el archivo, valido la extension, si está dentro de las validas lo elimino del servidor, caso contrario no porque evite se guarde con el multer
           fs.unlinkSync(req.file.path)
