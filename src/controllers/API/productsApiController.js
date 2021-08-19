1dce5f4aa52e01f8ea1873263e860d83e51da62d
UnDiaDeFuria <pabloar24.7@gmail.com>
2021-08-19 00:18:39 -0300

    api filtrado de productos por category

--- src/controllers/API/productsApiController.js
+++ src/controllers/API/productsApiController.js
@@ -1,4 +1,5 @@
 const { Product } = require('../../database/models')
+const { Category } = require('../../database/models')
 
 const controller = {
     list: async (req, res) => {
@@ -66,8 +67,29 @@ const controller = {
             totalProducts
         }
         res.json(response)
+    },
+    filterByCategory: async(req,res)=>{
+        let categoryToFind = req.params.category
+        let category = await Category.findOne({
+            where : {name : categoryToFind}
+        })
+        let products = await Product.findAll({
+            where :{
+                category_id: category.id
+            }
+        })
+        
+        let response = {
+            meta :{ products : products.length},
+            data: { products }
+           
+        }
+        res.json(response)
+
     }
+
 
 }
 
 module.exports = controller
+
--- src/routes/APIroutes/productsApiRoutes.js
+++ src/routes/APIroutes/productsApiRoutes.js
@@ -5,7 +5,9 @@ const productsApiController = require('../../controllers/API/productsApiControll
 //ruta para traer todos los usuarios
 router.get('/', productsApiController.list)
 router.get('/last',productsApiController.lastProduct)
+router.get('/category/:category', productsApiController.filterByCategory)
 router.get('/qty', productsApiController.qty)
 router.get('/:id', productsApiController.detail)
+
 
 module.exports = router
