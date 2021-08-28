e45a7daa2307cc10044b452fabcffccbff154c9b
FedeSeg <fedu.segura@gmail.com>
2021-08-28 17:21:14 -0300

    script en ejs y carturado elemntos del DOM user edit password

---
+++ public/js/user-edit-password-validation.js
@@ -0,0 +1,4 @@
+//CAPTURING DOM ELEMENTS
+const oldPassword = document.querySelector('#oldPassword');
+const password = document.querySelector('#password')
+const rePassword = document.querySelector('#rePassword')
