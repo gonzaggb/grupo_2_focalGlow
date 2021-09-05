7484f500366bcd61d431cf478b31ed5b606dbde8
gonzaggb <gonzaggb@hotmail.com>
2021-09-05 07:43:16 -0300

    escuchando evento change del options

--- public/js/product-detail.js
+++ public/js/product-detail.js
@@ -1,9 +1,56 @@
-const API_URL = `http://localhost:3000/api/products/detail/`
 
+const API_URL = `http://localhost:3000/api/products/detail/`
 //obtengo la ruta de la pagina
 const productDetailPath = window.location.pathname.split('/')
 //me quedo con el id de producto
-const productId = productDetailPath[productDetailPath.length-1]
+const productId = productDetailPath[productDetailPath.length - 1]
 
-console.log(productId)
-//fetch(`${API_URL}`)
+
+//captura features
+const powerSelect = document.querySelector('#power-select')
+const power = document.querySelectorAll('#power')
+
+
+
+
+const optic = document.querySelectorAll('#optic')
+const cct = document.querySelectorAll('#cct')
+const dim = document.querySelectorAll('#dim')
+
+
+//captura la feature del option
+function getFeatures(options) {
+    let selectedId = 0
+    options.forEach(element => {
+        element.selected === true ? selectedId = Number(element.value) : ''
+    })
+    return selectedId
+}
+
+//funcion para hacer fetch, al momento de usarla es necesario hacer un .then ya que es una promesa su respuesta
+async function getProducts(apiUrl) {
+    const response = await fetch(apiUrl)
+    const product = await response.json()
+    return product
+
+}
+
+
+
+
+//Obtengo de la DB las features y sus respectivos precios
+getProducts(`${API_URL}${productId}`).then(product => {
+    powerSelect.addEventListener('change', event => {
+
+        console.log(product)
+        product.data.features.forEach(feature => {
+            feature.id == getFeatures(power) ? price.innerHTML = Number(price.innerText.replace('$', '').replace('.', '')) + Number(feature.price) : ''
+        })
+
+    })
+})
+
+
+
+/* let originalPrice = price.innerText.replace('$', '')
+ */
